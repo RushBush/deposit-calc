976760199ef2a529c2026b1fdabd837e7647bd8d
Denis Belov <denisbelov9854@gmail.com>
2017-02-21 22:32:54 +0700

    Deposit calculations

--- main.c
+++ main.c
@@ -15,8 +15,31 @@ printf("Введите срок вклада "" ");
 scanf("%d", &time);
 while(time < 1 || time > 365)
 {
-  printf("Сумма вклада не может быть больше 365 дней и меньше 1 дня \n Повторите ввод "" ");
+  printf("Сумма вклада не может быть больше 365 дней и меньше 1 дня \n");
+  printf("Повторите ввод " "");
   scanf("%d", &time);
 }
-
+if (sum < 100000)
+ {
+   if (time > 0 && time < 31)
+       printf("Сумма с процентами=%.2f", (float) sum*0.9);
+   if (time > 30 && time < 121)
+       printf("Сумма с процентами=%.2f", (float) sum*1.02);
+   if (time > 120 && time < 241)
+       printf("Сумма с процентами=%.2f", (float) sum*1.06);
+   if (time > 240 && time < 366)
+       printf("Сумма с процентами=%.2f", (float) sum*1.12);
+ }
+ else
+ {
+   if (time > 0 && time < 31)
+       printf("Сумма с процентами=%.2f", (float) sum*0.9);
+   if (time > 30 && time < 121)
+       printf("Сумма с процентами=%.2f", (float) sum*1.03);
+   if (time > 120 && time < 241)
+       printf("Сумма с процентами=%.2f", (float) sum*1.08);
+   if (time > 240 && time < 366)
+       printf("Сумма с процентами=%.2f", (float) sum*1.15);
+ }
+return 0;
 }
