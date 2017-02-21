a2fe710d940ee5208db8250e1d291f2f7ab83b64
Denis Belov <denisbelov9854@gmail.com>
2017-02-21 22:04:52 +0700

    Проверка введенных данных

--- main.c
+++ main.c
@@ -3,10 +3,20 @@
 
 int main()
 {
-int sum, time, pers;
+int sum, time;
 printf("Введите сумму вклада "" ");
 scanf("%d", &sum);
+while(sum < 10000)
+{
+  printf("Неверная сумма \n Повторите ввод "" ");
+  scanf("%d", &sum);
+}
 printf("Введите срок вклада "" ");
 scanf("%d", &time);
+while(time < 1 || time > 365)
+{
+  printf("Сумма вклада не может быть больше 365 дней и меньше 1 дня \n Повторите ввод "" ");
+  scanf("%d", &time);
+}
 
 }
