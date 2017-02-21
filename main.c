b0b61a7e624d4402d286e1a93b3ef08d05413e13
Denis Belov <denisbelov9854@gmail.com>
2017-02-21 21:01:02 +0700

    Ввод данных

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int sum, time, pers;
+printf("Введите сумму вклада "" ");
+scanf("%d", &sum);
+printf("Введите срок вклада "" ");
+scanf("%d", &time);
+
+}
