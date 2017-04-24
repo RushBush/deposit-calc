72556c0244e69b6ec5257d7ec646fde1897bc171
rushbush54 <denisbelov9854@gmail.com>
2017-04-24 07:10:51 +0700

    Editing deposit.c/add check and calc functions

--- Makefile
+++ Makefile
@@ -1,2 +1,18 @@
-main: main.c
-		gcc -Wall main.c -o main
+CC = gcc
+BUILD_F = build/
+BIN_F = bin/
+SRC_F = src/
+SOURCES_O = main.o deposit.o
+KEYS = -Wall -Werror
+
+all:  deposit
+
+deposit: $(SOURCES_O)
+	$(CC) $(KEYS) $(BUILD_F)main.o $(BUILD_F)deposit.o -o $(BIN_F)deposit
+main.o : $(SRC_F)main.c
+	$(CC) $(KEYS) -c $(SRC_F)main.c -o $(BUILD_F)main.o
+deposit.o : $(SRC_F)deposit.c
+	$(CC) $(KEYS) -c $(SRC_F)deposit.c -o $(BUILD_F)deposit.o
+clean:
+	rm -rf $(BIN_F)*
+	rm -rf $(BUILD_F)*
--- src/deposit.c
+++ src/deposit.c
@@ -1,36 +1,41 @@
+
 #include "deposit.h"
-int CheckTerm(int a)
-{
-if (a<0 || a>365)
-return 0;
-else return a;
-}
-double CheckAmount(double b)
-{
-if (b<10000)
-return 0;
-else return b;
-}
-double Sum(int a,double b)
-{
-while(ChechkTerm(a)==0 || CheckAmount(b)==0)
-{
-printf("Not correct input,try again\n");
-scanf("%d %f",&a,&b);
-}
-if (b<100000.0)
- {
-  if (a<31) return b*0.9;
-  if (a>30 && a<121) return b*1.02;
-  if (a>120 && a<241) return b*1.06;
-  if (a>240 && a<366) return b*1.12;
- }
-if (b>100000.0)
-{
-  if (a<31) return b*0.9;
-  if (a>30 && a<121) return b*1.03;
-  if (a>120 && a<241) return b*1.08;
-  if (a>240 && a<366) return b*1.15;
+#include <stdio.h>
+
+int check(int day, float summa)
+{	
+	if ((day > 365) || (day < 0)) {
+		return -1;
+	}
+	if (summa < 10000) {
+		return -1;
+	}
+	return 0;
 }
 
+double calc(int day, int summa)
+{	
+	if (day < 31) 
+		return summa * 0.9;
+	if (summa <= 100000) {
+		if((day > 30) && (day < 121))
+			return summa * 1.02;
+
+		if((day > 120) && (day < 241))
+			return summa * 1.06;
+
+		if((day > 240) && (day < 366))
+			return summa * 1.12;
+	}
+	if (summa > 100000) {
+		if((day > 30) && (day < 121))
+			return summa * 1.03;
+
+		if((day > 120) && (day < 241))
+			return summa * 1.08;
+
+		if((day > 240) && (day < 366))
+			return summa * 1.15;
+	}
+	return 0;
 }
