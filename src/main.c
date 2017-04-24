7a163f37a325c4ba838bb5eaaca2e957b5aefc2e
rushbush54 <denisbelov9854@gmail.com>
2017-04-24 07:20:28 +0700

    Delete .swp files

--- src/main.c
+++ src/main.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "deposit.h"
+
 int main()
 {
-int a;
-double b;
-printf("Enter term and amount:\n");
-printf("%f",Sum(a,b));
+	int day, s;
+	float summa;
+	printf("Enter the term:  ");
+	scanf("%d", &day);
+	printf("Enter the amount: ");
+	scanf("%d", &summa);
+
+	while (check(day, summa) == -1) {
+		printf("Enter data again");
+		scanf("%d", &s);
+		if (s == 1) {
+			printf("Enter the term:  ");
+			scanf("%d", &day);
+	                printf("Enter the amount: ");
+			scanf("%d", &summa);
+		}
+		else return 0;
+	}
+	printf("Amount after deposit is %.3f\n", calc(day, summa));	
+	
+	return 0;
 }
